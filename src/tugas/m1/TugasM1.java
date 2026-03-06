5fccfa9fcafc9eaa0dffc272eaf35be8afc0362a
Willy-557 <williamfsetiadi@gmail.com>
2026-03-06 16:20:55 +0700

    Change the validation on Jual Produk

--- src/tugas/m1/TugasM1.java
+++ src/tugas/m1/TugasM1.java
@@ -227,42 +227,42 @@ public class TugasM1 {
                                 System.out.print("Jual berapa Chip? (maks: "+chip+"): ");
                                 int jualChip = scanner.nextInt();
                                 
-                                if (jualChip <= chip) {
+                                if (jualChip > chip || jualChip < 1) {
+                                    System.out.println("Tidak bisa dijual, dikarenakan melebih dari stock yang tersedia!");
+                                }
+                                else {
                                     int totalJualChip = 15 * jualChip;
                                     gold += totalJualChip;
                                     chip -= jualChip;
                                     System.out.println("Berhasil jual " + jualChip + " Chip! Gold bertambah " + totalJualChip + ".");
-                                }
-                                else {
-                                    System.out.println("Tidak bisa dijual, dikarenakan melebih dari stock yang tersedia!");
                                 }
                             }
                             else if (menuJual == 2){
                                 System.out.print("Jual berapa Cable? (maks: "+cable+"): ");
                                 int jualCable = scanner.nextInt();
                                 
-                                if (jualCable <= cable) {
+                                if (jualCable > cable || jualCable < 1) {
+                                    System.out.println("Tidak bisa dijual, dikarenakan melebih dari stock yang tersedia!");
+                                }
+                                else {
                                     int totalJualCable = 8 * jualCable;
                                     gold += totalJualCable;
                                     cable -= jualCable;
                                     System.out.println("Berhasil jual " + jualCable + " Cable! Gold bertambah " + totalJualCable + ".");
-                                }
-                                else {
-                                    System.out.println("Tidak bisa dijual, dikarenakan melebih dari stock yang tersedia!");
                                 }
                             }
                             else if (menuJual == 3){
                                 System.out.print("Jual berapa Screen? (maks: "+screen+"): ");
                                 int jualScreen = scanner.nextInt();
                                 
-                                if (jualScreen <= screen) {
+                                if (jualScreen > screen || jualScreen < 1) {
+                                    System.out.println("Tidak bisa dijual, dikarenakan melebih dari stock yang tersedia!");
+                                }
+                                else {
                                     int totalJualScreen = 25 * jualScreen;
                                     gold += totalJualScreen;
                                     screen -= jualScreen;
                                     System.out.println("Berhasil jual " + jualScreen + " Screen! Gold bertambah " + totalJualScreen + ".");
-                                }
-                                else {
-                                    System.out.println("Tidak bisa dijual, dikarenakan melebih dari stock yang tersedia!");
                                 }
                             }
                         }
