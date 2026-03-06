91bd257f2049cd60a84780ab81a240f2f73be01e
Willy-557 <williamfsetiadi@gmail.com>
2026-03-06 16:06:45 +0700

    Finish making the logic on the first menu (Kelola mesin)

--- src/tugas/m1/TugasM1.java
+++ src/tugas/m1/TugasM1.java
@@ -5,6 +5,7 @@
  */
 package tugas.m1;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -19,6 +20,7 @@ public class TugasM1 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner scanner = new Scanner (System.in);
+        Random rand = new Random();
         
         int shift = 1;
         int chip = 0;
@@ -32,16 +34,15 @@ public class TugasM1 {
             System.out.println("                 TECH FACTORY                ");
             System.out.println("=============================================");
             System.out.println("  Shift : " + shift + "    Gold : " + gold);
-            System.out.println("  Chip  : " + chip + "     Cable: " + cable);
-            System.out.println("  Screen: " + screen);
+            System.out.println("Chip: +" + chip +"  |  Cable: +"+ cable +"  |  Screen: +"+ screen +"  ");
             System.out.println("---------------------------------------------");
             System.out.println("  Slot Mesin: ");
             for (int i = 0; i < arrSlot.length; i++) {
                 if (arrSlot[i] == '\0') {
-                    System.out.print("  [ ]");
+                    System.out.print("[ ]");
                 }
                 else {
-                    System.out.print("  [ " + arrSlot[i] + " ]");
+                    System.out.print("[" + arrSlot[i] + "]");
                 }
                 if (i % 3 == 2) {
                     System.out.println();
@@ -63,15 +64,15 @@ public class TugasM1 {
                 System.out.println("  Gold AKhir  : " + gold);
                 System.out.println("--------------------------------------");
                 System.out.println("  Stok AKhir : ");
-                System.out.println("  Chip : " + chip +"Cable : " + cable + "Screen: " + screen);
+                System.out.println("  Chip : " + chip +"     Cable : " + cable + "     Screen: " + screen);
                 System.out.println("--------------------------------------");
                 System.out.println("  Komposisi Mesin Akhir: ");
                 for (int i = 0; i < arrSlot.length; i++) {
                     if (arrSlot[i] == '\0') {
-                        System.out.print("  [ ]");
+                        System.out.print("[ ]");
                     }
                     else {
-                        System.out.print("  [ " + arrSlot[i] + " ]");
+                        System.out.print("[" + arrSlot[i] + "]");
                     }
                     if (i % 3 == 2) {
                         System.out.println();
@@ -90,10 +91,10 @@ public class TugasM1 {
                         System.out.println("=== KELOLA MESIN ===");
                         for (int i = 0; i < arrSlot.length; i++) {
                             if (arrSlot[i] == '\0') {
-                                System.out.print("  [ ]");
+                                System.out.print("[ ]");
                             }
                             else {
-                                System.out.print("  [ " + arrSlot[i] + " ]");
+                                System.out.print("[" + arrSlot[i] + "]");
                             }
                             if (i % 3 == 2) {
                                 System.out.println();
@@ -102,9 +103,10 @@ public class TugasM1 {
                         System.out.print(" Slot Kosong : ");
                         for (int i = 0; i < arrSlot.length; i++) {
                             if (arrSlot[i] == '\0') {
-                                System.out.println(i);
+                                System.out.print(i + " ");
                             }
                         }
+                        System.out.println();
                         System.out.println("--------------------------------------------");
                         System.out.println("  1. Beli Mesin");
                         System.out.println("  0. kembali");
@@ -113,16 +115,16 @@ public class TugasM1 {
                         int menu1 = scanner.nextInt();
 
                         if (menu1 == 0) {
-                            System.out.println("Akan dikembalikan ke halaman utama!");
+                            System.out.println("Kembali ke menu utama.!");
                             break;
                         } 
                         else if (menu1 == 1) {
                             while (true) {
                                 System.out.println("=== BELI MESIN ===");
                                 System.out.println("  Pilih jenis mesin: ");
-                                System.out.println("  1. Chip Maker    ");
-                                System.out.println("  2. Chip Maker    ");
-                                System.out.println("  3. Chip Maker    ");
+                                System.out.println("  1. Chip Maker   (50G) - produksi 1-3 Chip/shift   @15G");
+                                System.out.println("  2. Cable Maker  (30G) - produksi 2-4 Cable/shift  @8G");
+                                System.out.println("  3. Screen Maker (80G) - produksi 1-2 Screen/shift @25G");
                                 System.out.println("  0. Batal");
 
                                 System.out.print(">> ");
@@ -132,21 +134,73 @@ public class TugasM1 {
                                     System.out.println("Akan dikembalikan ke halaman Kelola Mesin");
                                     break;
                                 }
-                                else if (menuBeliMesin == 1) {
-                                    
-                                }
-                                else {
+                                else if (menuBeliMesin < 1 || menuBeliMesin > 3){
                                     System.out.println("Error");
                                 }
-                            }
-                                
+                                else {
+                                    if (menuBeliMesin == 1) {
+                                        if (gold >= 50) {
+                                            System.out.print("Pilih slot kosong (0-5): ");
+                                            int slotChip = scanner.nextInt();
+
+                                            if (arrSlot[slotChip] == '\0') {
+                                                gold -= 50;
+                                                arrSlot[slotChip] = 'C';
+                                                System.out.println("Chip Maker berhasil dipasang di slot " + slotChip + "!");
+                                            }
+                                            else {
+                                                System.out.println("Slot ini telah ditempati oleh mesin lain");
+                                            }
+                                        }
+                                        else {
+                                            System.out.println("Gold anda tidak mencukupi untuk membeli mesin ini!");
+                                        }
+                                    }
+                                    else if (menuBeliMesin == 2) {
+                                        if (gold >= 30) {
+                                            System.out.print("Pilih slot kosong (0-5): ");
+                                            int slotCable = scanner.nextInt();
+
+                                            if (arrSlot[slotCable] == '\0') {
+                                                gold -= 30;
+                                                arrSlot[slotCable] = 'B';
+                                                System.out.println("Cable Maker berhasil dipasang di slot " + slotCable + "!");                                            
+                                            }
+                                            else {
+                                                System.out.println("Slot ini telah ditempati oleh mesin lain");
+                                            }
+                                        }
+                                        else {
+                                            System.out.println("Gold anda tidak mencukupi untuk membeli mesin ini!");
+                                        }
+                                    }
+                                    else if (menuBeliMesin == 3) {
+                                        if (gold >= 80) {
+                                            System.out.print("Pilih slot kosong (0-5): ");
+                                            int slotScreen = scanner.nextInt();
+
+                                            if (arrSlot[slotScreen] == '\0') {
+                                                gold -= 80;
+                                                arrSlot[slotScreen] = 'S';
+                                                System.out.println("Screen Maker berhasil dipasang di slot " + slotScreen + "!");
+                                            }
+                                            else {
+                                                System.out.println("Slot ini telah ditempati oleh mesin lain");
+                                            }
+                                        }
+                                        else {
+                                            System.out.println("Gold anda tidak mencukupi untuk membeli mesin ini!");
+                                        } 
+                                    }
+                                }
+                            }       
                         }
                         else {
                             System.out.println("Error");
                         }
-                    }
-                        
+                    }      
                 }
+//                
             }
         }
     }
